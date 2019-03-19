7515dd22ecd3453dc5f6c55daeac8e42d5ce1091
t7pash00 <t7pash00@students.oamk.fi>
2019-03-19 12:09:41 +0200

    giving labels W

--- app/src/main/java/advanced/android/ebcm/ResultGraph.java
+++ app/src/main/java/advanced/android/ebcm/ResultGraph.java
@@ -1,8 +1,10 @@
 package advanced.android.ebcm;
 
 import android.graphics.Color;
+import android.icu.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.ValueDependentColor;
 import com.jjoe64.graphview.series.BarGraphSeries;
@@ -18,8 +20,19 @@ public class ResultGraph extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_graph);
-
         GraphView graph = (GraphView) findViewById(R.id.graph);
+
+        graph.getGridLabelRenderer(). setLabelFormatter(new DefaultLabelFormatter() {
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                if (isValueX) {
+                    return super.formatLabel(value, isValueX);
+                } else {
+                    return super.formatLabel(value, isValueX) + "W";
+                }
+            }
+
+        });
 
         BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[]{
                 new DataPoint(0, 0),
@@ -46,6 +59,7 @@ public class ResultGraph extends AppCompatActivity {
         });
         graph.addSeries(series);
 
+
         series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
             @Override
             public int get(DataPoint data) {
@@ -55,6 +69,8 @@ public class ResultGraph extends AppCompatActivity {
         series.setSpacing(50);
         series.setDrawValuesOnTop(true);
         series.setValuesOnTopColor(Color.RED);
+
+
 
 
     }
