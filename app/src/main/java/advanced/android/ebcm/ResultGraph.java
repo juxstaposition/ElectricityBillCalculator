8799fe0a2bfe4100ab4388f19fd3d96eedc6b0d1
t7pash00 <t7pash00@students.oamk.fi>
2019-03-26 12:09:55 +0200

    giving color to the graph series

--- app/src/main/java/advanced/android/ebcm/ResultGraph.java
+++ app/src/main/java/advanced/android/ebcm/ResultGraph.java
@@ -4,13 +4,13 @@ import android.graphics.Color;
 import android.icu.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.Toast;
 import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.ValueDependentColor;
 import com.jjoe64.graphview.series.*;
 
-import java.security.KeyStore;
 
 public class ResultGraph extends AppCompatActivity {
 
@@ -52,44 +52,60 @@ GraphView graphView;
 
 
 
+
          BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[]{
-                new DataPoint(0, 0),
+                 new DataPoint(0, 0),
                 new DataPoint(1, 700),
-                new DataPoint(2, 3000),
-                new DataPoint(3, 2500),
-                new DataPoint(4, 60),
-                new DataPoint(5, 17060),
-                new DataPoint(6, 700),
+                new DataPoint(2, 100),
+                new DataPoint(3, 200),
+                new DataPoint(4, 90),
+                new DataPoint(5, 170),
+                new DataPoint(6, 7000),
                 new DataPoint(7, 0),
                 new DataPoint(8, 700),
-                new DataPoint(9, 3000),
+                new DataPoint(9, 300),
                 new DataPoint(10, 2500),
-                new DataPoint(11, 60),
+                new DataPoint(11, 25),
                 new DataPoint(12, 17060),
                 new DataPoint(13, 700),
                
         });
+
         graphView.addSeries(series);
+        series.setAnimated(true);
+
+
 series.setOnDataPointTapListener(new OnDataPointTapListener() {
     @Override
     public void onTap(Series series, DataPointInterface dataPoint) {
-        String masg = "X: "+dataPoint.getX()+"\nY: "+dataPoint.getY();
-        Toast.makeText(ResultGraph.this,masg,Toast.LENGTH_SHORT).show();
 
+        String masg = +dataPoint.getY() + "W";
+        Toast toast = Toast.makeText(ResultGraph.this,masg,Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0,0);
+        toast.show();
     }
-
 });
+
 
 
         series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
             @Override
             public int get(DataPoint data) {
-                return Color.rgb((int) data.getX() * 255/4, (int) Math.abs(data.getY() * 255 / 6), 100);
+                if (0 <= data.getY() && data.getY() < 1500)  {
+
+                    return Color.GREEN;
+
+                    } else if(1500<= data.getY() && data.getY()<8000){
+
+                    return Color.BLUE;
+                } else {
+                    return Color.RED;
+                }
             }
         });
         series.setSpacing(50);
         series.setDrawValuesOnTop(true);
-        series.setValuesOnTopColor(Color.RED);
+        series.setValuesOnTopColor(Color.BLACK);
 
 
 
