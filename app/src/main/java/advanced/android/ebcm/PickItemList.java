3fe85065cfaf6495a43a5f04ece266922093154b
t7pash00 <t7pash00@students.oamk.fi>
2019-04-10 11:06:24 +0300

    files created to work on pick an item

---
+++ app/src/main/java/advanced/android/ebcm/PickItemList.java
@@ -0,0 +1,13 @@
+package advanced.android.ebcm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickItemList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_item_list);
+    }
+}
